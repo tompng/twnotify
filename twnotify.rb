ddb13da80825951e69fa7c430faf804c13de608d
tompng <tomoyapenguin@gmail.com>
2014-09-21 16:48:46 +0900

    bugfix

--- twnotify.rb
+++ twnotify.rb
@@ -1,5 +1,6 @@
 require 'tweetstream'
 require 'http'
+require 'open-uri'
 TweetStream.configure do |config|
   config.consumer_key       = ENV['CONSUMER_KEY']
   config.consumer_secret    = ENV['CONSUMER_SECRET']
@@ -9,23 +10,30 @@ TweetStream.configure do |config|
 end
 
 class HashtagLoader
-  attr_accessor :client, :keywords, :callback, :tweet_id_history
+  attr_accessor :thread, :keywords, :callback, :tweet_id_history
   def initialize &block
     self.tweet_id_history = []
+    self.keywords = []
     self.callback = block
   end
   def track keywords
-    old_client = client
+    old_thread = thread
     return if keywords.sort == self.keywords.sort
     self.keywords = keywords
-    if keywords.present?
-      self.client = TweetStream::Client.new.track keywords do |status|
-        oncallback status
-      end
+    if keywords.empty?
+      self.thread = nil
     else
-      self.client = nil
+      self.thread = Thread.new{
+        TweetStream::Client.new.track *keywords do |status|
+          begin
+            oncallback status
+          rescue => e
+            p e
+          end
+        end
+      }
     end
-    old_client.stop if old_client
+    old_thread.exit if old_thread
   end
 
   def tweet_id_push id
@@ -46,13 +54,28 @@ class HashtagLoader
 end
 
 def get_keywords
-  JSON.parse HTTP.get(ENV['KEYWORDS_URL'])
+  if ENV['KEYWORDS_URL']
+    JSON.parse HTTP.get(ENV['KEYWORDS_URL']).body.to_s
+  else
+    gets.split
+  end
 end
 
 loader = HashtagLoader.new{|status|
-  HTTP.post(ENV['POST_URL'], status)
+  json = {
+    id: status.id,
+    text: status.text,
+    hashtags: status.hashtags.map(&:text),
+    user: {name: status.user.name, screen_name: status.user.screen_name}
+  }
+  if ENV['POST_URL']
+    HTTP.post ENV['POST_URL'], json: json
+  else
+    p json
+  end
 }
+
 loop{
-  sleep 10
   loader.track get_keywords
+  sleep 10
 }
